dbd88d390d36304a1214a423b2f54ede0ed79a12
ElenaAndreeva9 <elenaolen92@mail.ru>
2024-05-18 18:28:35 +0400

    Add Task_02

---
+++ src/Task_02.java
@@ -0,0 +1,12 @@
+public class Task_02 {
+    public static void main(String[] args) {
+        System.out.println("2");
+        int n = 865;
+        int x = n/100; //8
+        int y = n%10; //5
+        int z = n/10;//86
+        int zs = z%10; //6
+        int result = x+y+zs;
+        System.out.println(result);
+    }
+}
